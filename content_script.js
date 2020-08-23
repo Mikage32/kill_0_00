f586dc3297d1d3ee7a886283a5fbc749a0690bd3
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2020-08-23 20:54:50 +0900

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -8,12 +8,12 @@ function main(e) {
             var limit = document.querySelectorAll('td.cell.c1.lastcol')[2];
             var limit_value = limit.innerHTML;
             
-            console.log(limit_value.substr(limit_value.length-5, 5) === "00:00");
             if(limit_value.substr(limit_value.length-5, 5) === "00:00"){
                 limit_value = limit_value.substr(0, limit_value.length-5);
                 limit_value += "23:59";
                 let day = ["月", "火", "水", "木", "金", "土", "日"];
                 for(let i = 0; i < 7; ++i){
+                    console.log(limit_value.substr(12, 1));
                     if(limit_value.substr(12, 1) === day[i]){
                         limit_value.replace(day[i], day[(i+6)%7]);
                         break;
