744416cfd737cc6228acbd78ab0ab669199d2b70
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2020-08-23 19:25:30 +0900

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,1 +1,12 @@
-console.log("test");
+window.addEventListener("load", main, false);
+
+function main(e) {
+    const jsInitCheckTimer = setInterval(jsLoaded, 1000);
+    function jsLoaded() {
+        if (document.querySelector('main h1') != null) {
+            clearInterval(jsInitCheckTimer);
+            var title = document.querySelector('main h1');
+            console.log(x);
+        };
+    }
+}
