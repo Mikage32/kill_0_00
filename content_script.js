2eafc250b3d743d1ced37f74b49baee46549e73e
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2020-08-23 20:30:16 +0900

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -3,9 +3,9 @@ window.addEventListener("load", main, false);
 function main(e) {
     const jsInitCheckTimer = setInterval(jsLoaded, 1000);
     function jsLoaded() {
-        if (document.querySelectorAll('td.cell.c1.lastcol').length >= 2) {
+        if (document.querySelectorAll('td.cell.c1.lastcol').length >= 3) {
             clearInterval(jsInitCheckTimer);
-            document.querySelectorAll('td.cell.c1.lastcol')[1].innerHTML = "test";
+            document.querySelectorAll('td.cell.c1.lastcol')[2].innerHTML = "test";
         };
     }
 }
