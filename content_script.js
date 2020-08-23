4e31685dde9e8ca222978d74fabfe2c76703df12
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2020-08-23 20:24:22 +0900

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -4,7 +4,6 @@ function main(e) {
     const jsInitCheckTimer = setInterval(jsLoaded, 1000);
     function jsLoaded() {
         if (document.querySelector('td.cell.c1.lastcol') != null) {
-            alert("test");
             clearInterval(jsInitCheckTimer);
             document.querySelector('td.cell.c1.lastcol').innerHTML = "test";
         };
