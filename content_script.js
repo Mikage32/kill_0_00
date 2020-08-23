14f39c64e2fb814044327bc3955788ce2ad3d9dd
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2020-08-23 20:37:19 +0900

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -5,8 +5,13 @@ function main(e) {
     function jsLoaded() {
         if (document.querySelectorAll('td.cell.c1.lastcol').length >= 3) {
             clearInterval(jsInitCheckTimer);
-            var limit = document.querySelectorAll('td.cell.c1.lastcol')[2].innerHTML;
-            console.log(limit);
+            var limit = document.querySelectorAll('td.cell.c1.lastcol')[2];
+            var limit_value = limit.innerHTML;
+            
+            if(limit_value.substr(limit_value.length-5, 4) === "00:00"){
+                limit_value = limit_value.substr(0, limit_value.length-4);
+                limit_value += "23:59";
+            }
         };
     }
 }
