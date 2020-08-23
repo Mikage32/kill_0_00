925a97826c877145faf3f8878b35889384da4fed
Mikage32 <54508080+Mikage32@users.noreply.github.com>
2020-08-23 19:27:49 +0900

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -3,6 +3,8 @@ window.addEventListener("load", main, false);
 function main(e) {
     const jsInitCheckTimer = setInterval(jsLoaded, 1000);
     function jsLoaded() {
+        alert("test");
+        
         if (document.querySelector('main h1') != null) {
             clearInterval(jsInitCheckTimer);
             var title = document.querySelector('main h1');
